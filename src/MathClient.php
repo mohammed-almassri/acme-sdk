b58deeae69b46dd5406cc46ffebc782acfacad8b
Mohammed Almassri <mohammed.m.almassri@gmail.com>
2026-05-15 13:20:10 -0500

    feature

--- src/MathClient.php
+++ src/MathClient.php
@@ -22,6 +22,7 @@ final class MathClient
         if ($this->mathController === null) {
             $this->mathController = new MathController();
         }
+        $a = 1;
 
         return $this->mathController;
     }
